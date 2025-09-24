ddaf9c23f54002ad123c777e4fb367c9cb02aaf0
Go Yeongmin <ko07073@gmail.com>
2025-09-24 19:31:24 +0900

    first commit

---
+++ src/test/java/com/DBP/ticketing_backend/TicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBP.ticketing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
